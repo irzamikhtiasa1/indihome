42b107965e45e82d43c3c5cb36e20547fd4ddad3
irzamikhtiasa1 <81026189+irzamikhtiasa1@users.noreply.github.com>
2024-09-07 11:12:33 +0700

    Update orbit.purches.js

--- orbit.purches.js
+++ orbit.purches.js
@@ -1,14 +1,4 @@
-// Menampilkan dan menyembunyikan daftar kontak
-document.getElementById('contactButton').addEventListener('click', function() {
-    const contactList = document.getElementById('contactList');
-    if (contactList.style.display === 'block') {
-        contactList.style.display = 'none';
-    } else {
-        contactList.style.display = 'block';
-    }
-});
-
-// Fungsi untuk membuka link WhatsApp dengan pesan default
+/ Fungsi untuk membuka link WhatsApp dengan pesan default
 function sendMessage(phoneNumber) {
     const message = "Halo MinHome, saya tertarik ingin berlangganan Orbit. Tapi saya ingin tanya dulu.";
     const encodedMessage = encodeURIComponent(message);
@@ -16,7 +6,4 @@ function sendMessage(phoneNumber) {
 
     // Arahkan pengguna ke WhatsApp
     window.open(waLink, '_blank');
-
-    // Sembunyikan daftar kontak setelah dipilih
-    document.getElementById('contactList').style.display = 'none';
 }

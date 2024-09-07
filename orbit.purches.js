69700f23c46d099253b5b1664981d84105b7fb00
irzamikhtiasa1 <81026189+irzamikhtiasa1@users.noreply.github.com>
2024-09-07 11:23:57 +0700

    Update orbit.purches.js

--- orbit.purches.js
+++ orbit.purches.js
@@ -1,4 +1,3 @@
-/ Fungsi untuk membuka link WhatsApp dengan pesan default
 function sendMessage(phoneNumber) {
     const message = "Halo MinHome, saya tertarik ingin berlangganan Orbit. Tapi saya ingin tanya dulu.";
     const encodedMessage = encodeURIComponent(message);
